6f3fcbebf4c994feec6a698feb7ef29c2a757e4a
Taha Eid <10812022101005@stud.cu.edu.eg>
2025-10-17 16:54:18 +0300

    Add SimpleApp with unit tests

---
+++ src/test/java/org/example/SimpleAppTesting.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import com.example.App;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTesting {
+
+    private App calculator = new App();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(2, -3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+        assertEquals(5, calculator.subtract(2, -3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+        assertEquals(0, calculator.multiply(5, 0));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, calculator.divide(6, 3));
+        assertEquals(2.5, calculator.divide(5, 2));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(5, 0);
+        });
+    }
+}
